28f5e9cc31006687f1122a863ce0daedf1a25a40
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2022-12-06 17:10:06 +0300

    feat: added solution HW5

--- src/s2-homeworks/hw05/HW5.tsx
+++ src/s2-homeworks/hw05/HW5.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import { HashRouter } from 'react-router-dom'
-import { Layout } from './layout/Layout'
+import {HashRouter} from 'react-router-dom'
+import {Layout} from './layout/Layout'
 import Pages from './Pages'
 
 /*
@@ -12,9 +12,8 @@ import Pages from './Pages'
 function HW5() {
     return (
         <HashRouter>
-            {/*в gh-pages лучше работает HashRouter, с BrowserRouter скорее всего не пройдёт тест*/}
             <Layout>
-                <Pages />
+                <Pages/>
             </Layout>
         </HashRouter>
     )
