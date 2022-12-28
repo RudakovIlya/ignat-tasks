f1d07fdda2173c7279ab2c13a927aa57e5c517d6
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2022-12-28 15:02:10 +0300

    feat: add solution HW10

--- src/s2-homeworks/hw10/HW10.tsx
+++ src/s2-homeworks/hw10/HW10.tsx
@@ -14,13 +14,18 @@ import {Loader} from './Loader'
 * */
 
 const HW10 = () => {
-    // useSelector, useDispatch // пишет студент
-    const isLoading = false
 
-    const setLoading = () => { // пишет студент // показать крутилку на 1,5 секунд
-        // dispatch
+    const isLoading = useSelector((state: AppStoreType) => state.loading.isLoading)
 
-        // setTimeout
+    const dispatch = useDispatch();
+    const setLoading = () => {
+
+        dispatch(loadingAC(isLoading));
+
+        setTimeout(() => {
+            dispatch(loadingAC(isLoading));
+        }, 1500)
+
     }
 
     return (
--- src/s2-homeworks/hw10/bll/loadingReducer.ts
+++ src/s2-homeworks/hw10/bll/loadingReducer.ts
@@ -2,10 +2,13 @@ const initState = {
     isLoading: false,
 }
 
-export const loadingReducer = (state = initState, action: any): any => { // fix any
+export const loadingReducer = (state = initState, action: LoadingActionType): typeof state => {
     switch (action.type) {
-        // пишет студент  // need to fix
-
+        case "CHANGE_LOADING":
+            return {
+                ...state,
+                isLoading: !state.isLoading
+            }
         default:
             return state
     }
