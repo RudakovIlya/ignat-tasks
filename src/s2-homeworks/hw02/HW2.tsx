87e708a3b2adfd1d283cbfed4737085ded12c2f3
Ilya Rudakov <96202997+RudakovIlya@users.noreply.github.com>
2023-01-03 22:24:18 +0300

    fix: bugs in HW02

--- src/s2-homeworks/hw02/HW2.tsx
+++ src/s2-homeworks/hw02/HW2.tsx
@@ -50,7 +50,7 @@ function HW2() {
     const filteredAffairs = filterAffairs(affairs, filter);
 
     const deleteAffairCallback = (_id: number) => {
-        setAffairs(deleteAffair(filteredAffairs, _id));
+        setAffairs(deleteAffair(affairs, _id));
     };
 
     return (
